0230fdd2740398ae5fabe4798380798962452a51
Maksym Skalskyi <skalskyymax@gmail.com>
2023-07-24 03:59:44 +0200

    1.01

--- src/main/java/com/example/jfi/servlets/InitServlet.java
+++ src/main/java/com/example/jfi/servlets/InitServlet.java
@@ -1,5 +1,8 @@
 package com.example.jfi.servlets;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.*;
 
 import javax.servlet.ServletException;
@@ -10,14 +13,24 @@ import javax.servlet.http.HttpServletResponse;
 
 import static com.example.jfi.classes.User.getUser;
 
-
 @WebServlet(name = "InitServlet", value = "/init")
 public class InitServlet extends HttpServlet {
+
+    private static final Logger log = LogManager.getLogger(InitServlet.class);
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        getUser().setName(request.getParameter("name"));
-        getUser().setIp(String.valueOf(request.getRemoteAddr()));
-        getUser().plusCountGame();
-        request.getSession().setAttribute("user", getUser());
-        getServletContext().getRequestDispatcher("/story.jsp").forward(request, response);
-   }
+        log.info("InitServlet - doGet method started.");
+
+        try {
+            getUser().setName(request.getParameter("name"));
+            getUser().setIp(String.valueOf(request.getRemoteAddr()));
+            getUser().plusCountGame();
+            request.getSession().setAttribute("user", getUser());
+            getServletContext().getRequestDispatcher("/story.jsp").forward(request, response);
+
+            log.info("InitServlet - doGet method completed successfully.");
+        } catch (Exception e) {
+            log.error("InitServlet - Error in doGet method: ", e);
+        }
+    }
 }
